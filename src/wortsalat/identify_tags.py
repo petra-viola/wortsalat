37d1e8c9aac7ab969fe20d984c56687f715e1a19
petra-viola <118911836+petra-viola@users.noreply.github.com>
2023-12-20 09:14:59 +0100

    trty docstring extension

--- src/wortsalat/identify_tags.py
+++ src/wortsalat/identify_tags.py
@@ -8,6 +8,28 @@ def identify_tags(tag: str, text: str, taglevel: int = 0) -> Dict[str, List[str]
     """
     Identify words in a given text that match a specific POS tag.
 
+    Parameters:
+    - tag (str): The POS tag to match.
+    - text (str): The input text for which the POS tags will be identified.
+    - taglevel (int): The level of detail of the POS tags to return. Default is 0.
+
+    Returns:
+    - Dict[str, List[str]]: A dictionary where each key is a POS tag and each value is a list of words that were assigned that tag.
+    """
+    words = tokenize_words(text)
+    tagged_words = tagger_de.tag_sent(words, taglevel=0)
+
+    words_with_tag = []
+
+    for word, pos_tag in tagged_words:
+        if pos_tag == tag:
+            words_with_tag.append(word)
+
+    return words_with_tag
+
+    """
+    Identify words in a given text that match a specific POS tag.
+
     This function tokenizes the input text, tags the words using the HanTa library, and then identifies the words that match the specified POS tag.
 
     tags = {
@@ -29,13 +51,3 @@ def identify_tags(tag: str, text: str, taglevel: int = 0) -> Dict[str, List[str]
     Returns:
     - Dict[str, List[str]]: A dictionary where each key is a POS tag and each value is a list of words that were assigned that tag.
     """
-    words = tokenize_words(text)
-    tagged_words = tagger_de.tag_sent(words, taglevel=0)
-
-    words_with_tag = []
-
-    for word, pos_tag in tagged_words:
-        if pos_tag == tag:
-            words_with_tag.append(word)
-
-    return words_with_tag
--- src/wortsalat/identify_words.py
+++ src/wortsalat/identify_words.py
@@ -8,32 +8,3 @@ def identify_words(type: str ,text: str) -> int:
         dictionary = [line.strip("\n") for line in dictionary]
     count = sum(1 for word in text.split() if word in dictionary)
     return count
-
-'''from importlib import resources as impresources
-from wortsalat.preprocess import tokenize_words
-from . import data
-
-def identify_words(type: str, text: str) -> dict:
-    """
-    Identify words in a text that are present in a specific dictionary.
-
-    This function reads a dictionary from a file, and then checks if each word in the provided text is present in the dictionary.
-    The dictionary file is expected to be located in the 'data' package, and the type of dictionary is specified by the 'type' parameter.
-
-    Parameters:
-    - type (str): The type of dictionary to use. This should correspond to a file in the 'data' package.
-    - text (str): The text to check for words present in the dictionary.
-
-    Returns:
-    - Dict[str, list]: A dictionary where each key is a word from the text, and the value is a list containing that word if it was found in the dictionary.
-    """
-    inp_file = (impresources.files(data) / type)
-    with inp_file.open("rt") as f:
-        dictionary = f.readlines()
-        dictionary = [line.strip("\n") for line in dictionary]
-    words = tokenize_words(text)
-    word_lists = {word: [] for word in words}
-    for word in words:
-        if word in dictionary:
-            word_lists[word].append(word)
-    return word_lists'''
